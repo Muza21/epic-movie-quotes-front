7fdc47384cb8c2e3f80ba3f75fe0cc90587a2cea
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-11-21 18:34:35 +0400

    fix: remove comments

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -12,7 +12,6 @@
           >
             Sign Up
           </router-link>
-
           <router-link
             :to="{ name: 'login' }"
             class="mx-2 px-4 py-2 rounded-lg text-white border border-white"
@@ -38,56 +37,17 @@
       </router-link>
     </div>
   </div>
-  <v-modal :open="open" @close="open = false">
-    <template v-slot:title v-if="open">
-      {{ modalContents[currentModal].title }}
-    </template>
-    <template v-slot:content v-if="open">
-      {{ modalContents[currentModal].content }}
-    </template>
-  </v-modal>
   <ModalView :modalActive="modalActive">
     <router-view />
   </ModalView>
   <img src="src/assets/Rectangle.jpg" alt="pic" />
-  <!--<div class="border-8 border-red-500">
-    <SignUp />
-  </div>
-  <div class="border-8 border-red-500">
-    <LogIn />
-  </div>
-  <div class="border-8 border-red-500">
-    <ForgotPassword />
-  </div>
-  <div class="border-8 border-red-500">
-    <NewPassword />
-  </div>
-  <div class="border-8 border-red-500">
-    <ResetPassword />
-  </div>
-  <div class="border-8 border-red-500">
-    <RegisteredSuccessfully />
-  </div>
-  <div class="border-8 border-red-500">
-    <VerifiedSuccessfully />
-  </div>
-  <div class="border-8 border-red-500">
-    <ResetedPassword />
-  </div> -->
+
   <footer class="py-4 px-10 bg-[#11101A]">
     <div class="text-[#DDCCAA]">&#169; MOVIE QUOTES. ALL RIGHTS RESERVED.</div>
   </footer>
 </template>
 
 <script>
-// import SignUp from "@/components/SignUp.vue";
-// import LogIn from "@/components/LogIn.vue";
-// import ForgotPassword from "@/components/ForgotPassword.vue";
-// import NewPassword from "@/components/NewPassword.vue";
-// import ResetPassword from "@/components/notifications/ResetPassword.vue";
-// import RegisteredSuccessfully from "@/components/notifications/RegisteredSuccessfully.vue";
-// import VerifiedSuccessfully from "@/components/notifications/VerifiedSuccessfully.vue";
-// import ResetedPassword from "@/components/notifications/ResetedPassword.vue";
 import ModalView from "@/components/ModalView.vue";
 import { ref } from "vue";
 
@@ -97,14 +57,6 @@ export default {
   },
 
   components: {
-    // SignUp,
-    // LogIn,
-    // ForgotPassword,
-    // NewPassword,
-    // ResetPassword,
-    // RegisteredSuccessfully,
-    // VerifiedSuccessfully,
-    // ResetedPassword,
     ModalView,
   },
   setup() {
