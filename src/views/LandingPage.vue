991bea6d287352a93886e4eb56cf1901c7248fe0
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-11-21 08:21:00 +0400

    remove hello world and add landing page basic style

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="py-4 px-10 bg-[#11101A]">
+    <div class="wrapper">
+      <nav class="flex justify-between">
+        <RouterLink to="/" class="text-[#DDCCAA]">MOVIE QUOTES</RouterLink>
+        <div>
+          <span class="mx-2 text-white">Eng</span>
+          <span class="mx-2 bg-[#E31221] px-4 py-2 rounded-lg text-white"
+            >Sign Up</span
+          >
+          <span class="mx-2 px-4 py-2 rounded-lg text-white border border-white"
+            >Log In</span
+          >
+        </div>
+      </nav>
+    </div>
+  </header>
+  <div class="bg-[#11101A] py-36">
+    <h1 class="text-center text-3xl text-[#DDCCAA]">This is an landing page</h1>
+  </div>
+  <img src="src/assets/Rectangle.jpg" alt="pic" />
+</template>
