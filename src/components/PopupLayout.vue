62eaf20709e8442a7711956234b6e33c1ad6afe9
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-11-21 23:18:48 +0400

    fix: background blur effect

--- src/components/PopupLayout.vue
+++ src/components/PopupLayout.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="bg-black bg-opacity-70 fixed w-[100vw] h-[100vh] top-0 left-0">
+  <div
+    class="bg-black bg-opacity-60 backdrop-blur-sm fixed w-[100vw] h-[100vh] top-0 left-0"
+  >
     <div
       class="container fixed max-w-full sm:max-w-4xl sm:h-fit bg-[#222030] sm:py-24 px-6 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]"
     >
