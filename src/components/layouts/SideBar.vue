7c40b5288da34ec795bc8094dbcdcec793b422c8
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-11-26 12:55:58 +0400

    feat: add profile basic style

--- src/components/layouts/SideBar.vue
+++ src/components/layouts/SideBar.vue
@@ -5,11 +5,9 @@
     <div class="divide-y divide-gray-700">
       <ul class="pt-2 pb-4 space-y-1 text-sm">
         <li class="flex dark:bg-gray-800 dark:text-gray-50">
-          <img
-            src="src/assets/Ellipse 1.png"
-            alt=""
-            class="w-12 h-12 rounded-full dark:bg-gray-500"
-          />
+          <div class="rounded-full overflow-hidden bg-red-400 w-12 h-12">
+            <img alt="..." src="/src/assets/ProfilePic.jpg" />
+          </div>
           <div class="ml-4">
             <h2 class="text-lg font-semibold text-white">Nino Tabagari</h2>
             <span class="flex items-center space-x-1">
--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,7 +1,93 @@
 <template>
-  <div class="bg-[#12101A] h-[100vh]">
+  <div class="bg-[#222030] h-[100vh]">
     <NavigationBar />
-    <SideBar />
+    <div class="flex flex-col justify-between">
+      <div class="flex">
+        <div class="flex flex-col h-screen overflow-y-auto">
+          <div class="flex flex-col justify-between">
+            <SideBar />
+          </div>
+        </div>
+        <div
+          class="w-full h-full md:w-[1000px] mx-auto bg-[#11101A] rounded-lg m-24 overflow-y-auto"
+        >
+          <div class="flex items-center justify-center py-16 px-52">
+            <div class="px-6 w-full">
+              <div class="flex flex-wrap justify-center">
+                <div
+                  class="w-full px-4 lg:w-3/12 lg:order-2 flex justify-center"
+                >
+                  <div class="relative">
+                    <div
+                      class="rounded-full overflow-hidden bg-red-400 w-32 h-32"
+                    >
+                      <img alt="..." src="/src/assets/ProfilePic.jpg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div
+                  class="text-center text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase"
+                >
+                  Upload new photo
+                </div>
+                <div class="mb-2 text-gray-700 mt-10">
+                  <div class="py-1">
+                    <span class="px-1 text-sm text-white">Username</span>
+                    <div class="flex justify-between items-center">
+                      <input
+                        placeholder=""
+                        type="text"
+                        class="text-md block px-3 py-2 rounded-lg w-full bg-[#CED4DA] border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+                      />
+                      <div class="ml-4 text-[#CED4DA]">Edit</div>
+                    </div>
+                  </div>
+                  <div class="my-10 border-t border-[#CED4DA]"></div>
+                  <div class="py-1">
+                    <span class="px-1 text-sm text-white">Email</span>
+                    <div class="flex justify-between items-center">
+                      <input
+                        placeholder=""
+                        type="email"
+                        class="text-md block px-3 py-2 rounded-lg w-full bg-[#CED4DA] border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+                      />
+                      <div class="ml-4 text-[#CED4DA]">Edit</div>
+                    </div>
+                  </div>
+                  <button
+                    class="mt-10 px-4 py-2 rounded-lg text-white border border-white"
+                  >
+                    Add new email
+                  </button>
+                  <div class="my-10 border-t border-[#CED4DA]"></div>
+
+                  <div class="py-1">
+                    <span class="px-1 text-sm text-white">Password</span>
+                    <div class="flex justify-between items-center">
+                      <input
+                        placeholder=""
+                        type="password"
+                        x-model="password"
+                        class="text-md block px-3 py-2 rounded-lg w-full bg-[#CED4DA] border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+                      />
+                      <div class="ml-4 text-[#CED4DA]">Edit</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="pt-4 bg-[#222030] flex justify-end">
+            <button class="mx-4 px-4 py-2 rounded-lg text-white">Cancel</button>
+            <button class="mx-4 bg-[#E31221] px-4 py-2 rounded-lg text-white">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
