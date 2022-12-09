ab6cd7ac5c8d93bd6f7eb6a287a9a1c37e51f884
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-12-09 14:46:54 +0400

    fix: genre selector

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -39,13 +39,39 @@
           </div>
         </div>
         <div class="py-1">
-          <div class="p-2 flex items-center w-full border-2 border-[#6C757D]">
-            <div class="mx-3 flex-1">
-              <div class="flex items-center">
+          <div
+            @click="toggleGenres"
+            class="p-2 flex items-center w-full border-2 h-14 border-[#6C757D]"
+          ></div>
+          <div class="mx-3 flex-1 absolute -mt-[51px]">
+            <div class="flex items-center">
+              <div v-for="genre in reactiveSelectedGenres.values" :key="genre">
                 <div class="p-2 ml-2 bg-[#6C757D] text-white flex items-center">
-                  Genre name <IconCross class="w-3 h-3" />
+                  {{ genre }}
+
+                  <div @click="deleteGenre" class="p-2 cursor-pointer">
+                    <IconCross class="w-3 h-3" />
+                  </div>
                 </div>
-                <!-- <Field name="picture" type="file" class="hidden" /> -->
+              </div>
+            </div>
+          </div>
+          <div>
+            <div
+              v-if="chooseGenres"
+              class="absolute w-60 rounded-lg bg-[#11101A] mt-2 h-96 overflow-y-auto border-2 border-gray-500"
+            >
+              <div v-for="genre in genres.values" :key="genre">
+                <div class="py-1 hover:bg-gray-500">
+                  <Field
+                    @click="selectGenre"
+                    class="p-3 rounded-md w-60 text-white bg-[#11101A] cursor-pointer outline-none"
+                    type="text"
+                    :name="genre.name"
+                    :value="genre.name"
+                    readonly
+                  />
+                </div>
               </div>
             </div>
           </div>
@@ -85,6 +111,20 @@
             rules="required"
             class="text-white text-lg block px-3 py-2 rounded-lg w-full bg-[#11101A] border-2 border-[#6C757D] placeholder-white shadow-md"
           />
+          <div>
+            <ErrorMessage class="ml-4 text-orange-600" name="year" />
+          </div>
+        </div>
+        <div class="py-1">
+          <Field
+            placeholder="Budget"
+            name="budget"
+            rules="required"
+            class="text-white text-lg block px-3 py-2 rounded-lg w-full bg-[#11101A] border-2 border-[#6C757D] placeholder-white shadow-md"
+          />
+          <div>
+            <ErrorMessage class="ml-4 text-orange-600" name="budget" />
+          </div>
         </div>
         <div class="py-1">
           <Field
@@ -131,7 +171,6 @@
                   class="hidden"
                   accept="image/jpeg, image/png"
                   rules="required"
-                  @change="onFileSelected"
                 />
                 <label
                   for="movie_picture"
@@ -163,12 +202,32 @@ import IconCross from "@/components/icons/IconCross.vue";
 import { Form as ValidationForm, Field, ErrorMessage } from "vee-validate";
 import axiosInstance from "@/config/axios/index.js";
 import { useRouter } from "vue-router";
+import { onMounted, ref, reactive } from "vue";
 
 const router = useRouter();
 
-function onFileSelected(e) {
-  console.log(e.target.files[0]);
-}
+const genres = reactive({});
+const chooseGenres = ref(false);
+const selectedGenres = ref([]);
+
+const reactiveSelectedGenres = reactive({});
+
+const toggleGenres = () => {
+  chooseGenres.value = !chooseGenres.value;
+};
+
+const selectGenre = (e) => {
+  if (selectedGenres.value.indexOf(e.target.name) === -1) {
+    selectedGenres.value.push(e.target.name);
+  }
+  chooseGenres.value = false;
+  reactiveSelectedGenres.values = selectedGenres.value;
+  console.log(reactiveSelectedGenres.values);
+};
+
+const deleteGenre = (e) => {
+  console.log(e);
+};
 
 const onSubmit = async (values) => {
   try {
@@ -177,14 +236,13 @@ const onSubmit = async (values) => {
     formData.append("movie_name_ka", values.movie_name_ka);
     formData.append("director_name_en", values.director_name_en);
     formData.append("director_name_ka", values.director_name_ka);
-    formData.append("genre", values.genre);
+    formData.append("genre", selectedGenres.value);
     formData.append("year", values.year);
     formData.append("budget", values.budget);
     formData.append("movie_description_en", values.movie_description_en);
     formData.append("movie_description_ka", values.movie_description_ka);
     formData.append("movie_picture", values.movie_picture);
 
-    console.log(formData);
     const response = await axiosInstance.post(`/add-movie`, formData, {
       headers: {
         "Content-Type": "multipart/form-data;",
@@ -196,4 +254,16 @@ const onSubmit = async (values) => {
     console.log(err);
   }
 };
+
+onMounted(() => {
+  axiosInstance
+    .get(`/genres`)
+    .then((response) => {
+      genres.values = response.data.genres;
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 </script>
